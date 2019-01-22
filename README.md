0b4573b0597548f757db41fdb779b74ff172fd7f
Michele Riva <master@micheleriva.it>
2019-01-22 11:30:43 +0100

    wip

---
+++ README.md
@@ -0,0 +1,4 @@
+<h1 align="center"> 🐷 Salvini-Blocker 🐷 </h1>
+<p align="center"> Like Ad-Block, but for Matteo Salvini  </p>
+
+<img src="/docs/Salvini-Block.png" align="center" />
--- main.js
+++ main.js
@@ -1,16 +1,31 @@
 (function disableSalveenee() {
 
-  const images = document.querySelectorAll("img")
+  const imgTag    = document.querySelectorAll("img")
+  const sourceTag = document.querySelectorAll("source")
   const filteredImages = []
 
-  for (let image of images) {
+  for (let image of imgTag) {
+    if (image.outerHTML.match(/salvini/gi)) {
+      filteredImages.push(image)
+    }
+  }
+
+  for (let image of sourceTag) {
     if (image.outerHTML.match(/salvini/gi)) {
       filteredImages.push(image)
     }
   }
 
   for (let node of filteredImages) {
-    node.src = "https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=400"
+    
+    const newImage = (node) => `https://images.pexels.com/photos/127028/pexels-photo-127028.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=${node.clientWidth || 400}&h=${node.clientHeight || 250}`
+
+    if(node.src) {
+      node.src = newImage(node)
+    } else if (node.srcset) {
+      node.srcset = newImage(node)
+    }
+
   }
 
 })()
